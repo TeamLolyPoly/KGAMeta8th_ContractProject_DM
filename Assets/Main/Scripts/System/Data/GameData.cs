108ff3c74714094592bc71de0658e7bb933685a1
KimJin <khkcejkms1@naver.com>
2025-02-27 11:47:06 +0900

    [feat] NoteManager만들었습니다
    [body]
    - 기존 오른쪽 왼쪽 노트 통합했습니다.
    - 노트 매니저 만들어서 스코어 연결했습니다.
    - 노트 점수 계산시스템 완성했습니다.

--- Assets/Main/Scripts/System/Data/GameData.cs
+++ Assets/Main/Scripts/System/Data/GameData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class NoteData
 {
-    public HitType noteType;
+    public NoteHitType noteType;
     public NoteDirection direction;
     public NoteAxis noteAxis;
     public Vector3 target;
