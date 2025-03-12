64390c7c4084618fcae15846e5498539daeaad97
khj9178 <h91789084@gmail.com>
2025-03-12 09:35:29 +0900

    [feat] UI 기본구조 구현중
    [body]
    -구현중입니다

---
+++ Assets/Main/Scenes/Devs/HJKim/script/AlbumPanel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using ProjectDM;
+using ProjectDM.UI;
+using UnityEngine;
+
+public class AlbumPanel : Panel
+{
+    public override PanelType PanelType => PanelType.Album;
+
+    public override void Open()
+    {
+        base.Open();
+    }
+}
--- Assets/Main/Scenes/Devs/HJKim/script/ModePanel.cs
+++ Assets/Main/Scenes/Devs/HJKim/script/ModePanel.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Michsky.UI.Heat;
 using ProjectDM.UI;
-using UnityEngine;
 
 public class ModePanel : Panel
 {
