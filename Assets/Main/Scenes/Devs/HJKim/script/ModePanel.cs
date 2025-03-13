299cb1694e244d8b265f96590bffabf24ee8ee54
khj9178 <h91789084@gmail.com>
2025-03-13 17:35:50 +0900

    [feat]1차로 UIManager 이랑 타입패널 연결했습니다.
    [body]
    -싱글은 패널 연결까지 가능
    - 아직 추가패널은 없음
    
    [todo]
    @UIManager
    -(issue)Heat UI 오류
    -Heat UI 프리팹 이용해서 버튼 오류나는거 교체
    -스크롤 기능
    -게임스타트 조건

--- Assets/Main/Scenes/Devs/HJKim/script/ModePanel.cs
+++ Assets/Main/Scenes/Devs/HJKim/script/ModePanel.cs
@@ -21,17 +21,31 @@ public class ModePanel : Panel
     public override void Open()
     {
         backButton.onClick.AddListener(OnBackButtonClick);
+        singleButton.onClick.AddListener(OnSingleButtonClick);
+        multiButton.onClick.AddListener(OnMultiButtonClick);
         base.Open();
     }
 
     public override void Close()
     {
         backButton.onClick.RemoveListener(OnBackButtonClick);
+        singleButton.onClick.RemoveListener(OnSingleButtonClick);
+        multiButton.onClick.RemoveListener(OnMultiButtonClick);
         base.Close();
     }
 
     private void OnBackButtonClick()
     {
         UIManager.Instance.OpenPanel(PanelType.Title);
+    }
+
+    private void OnSingleButtonClick()
+    {
+        UIManager.Instance.OpenPanel(PanelType.Album);
+    }
+
+    private void OnMultiButtonClick()
+    {
+        //todo : 멀티모드 구현해야함  기획서 참고!
     }
 }
--- Assets/Main/Scenes/Devs/HJKim/script/OptionPanel.cs
+++ Assets/Main/Scenes/Devs/HJKim/script/OptionPanel.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Michsky.UI.Heat;
 using ProjectDM.UI;
 using UnityEngine;
 
 public class OptionPanel : Panel
 {
     public override PanelType PanelType => PanelType.Option;
+
+    [SerializeField]
+    private PanelButton backButton;
+
+    public override void Open()
+    {
+        backButton.onClick.AddListener(OnBackButtonClick);
+        base.Open();
+    }
+
+    public override void Close()
+    {
+        backButton.onClick.RemoveListener(OnBackButtonClick);
+        base.Close();
+    }
+
+    private void OnBackButtonClick()
+    {
+        UIManager.Instance.OpenPanel(PanelType.Title);
+    }
 }
