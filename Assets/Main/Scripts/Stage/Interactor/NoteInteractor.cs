c9da8e53494a492579efb1595f40f8e7972bf6c0
beooom <qjarns1423@gmail.com>
2025-04-08 09:52:36 +0900

    [chore] NoteInteractor photonview 추가
    [body]
    - Note Interactor에 Phtonview 멤버 추가

--- Assets/Main/Scripts/Stage/Interactor/NoteInteractor.cs
+++ Assets/Main/Scripts/Stage/Interactor/NoteInteractor.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
 
 public class NoteInteractor : MonoBehaviour
 {
     public NoteColor noteColor;
     private XRBaseController controller;
+    private PhotonView photonView;
 
     private void Awake()
     {
         controller = GetComponentInParent<XRBaseController>();
+        photonView = GetComponentInParent<PhotonView>();
     }
 
     public void SendImpulse()
