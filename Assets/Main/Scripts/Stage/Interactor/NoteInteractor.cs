ce21bf501519f97092e57b974284747bdef7ccbc
beooom <qjarns1423@gmail.com>
2025-04-09 14:20:31 +0900

    [feat] Remote, Loacal PhotonView Player Rig sync
    [body]
    - 플레이어 위치 동기화 로직 수정
    - 멀티, 로컬 플레이어 PhotonView를 통한 동기화 작업하도록 수정

--- Assets/Main/Scripts/Stage/Interactor/NoteInteractor.cs
+++ Assets/Main/Scripts/Stage/Interactor/NoteInteractor.cs
@@ -8,6 +8,9 @@ public class NoteInteractor : MonoBehaviour
     private XRBaseController controller;
     private PhotonView photonView;
 
+    [SerializeField]
+    private GameObject hitFXPrefab;
+
     private void Awake()
     {
         controller = GetComponentInParent<XRBaseController>();
@@ -17,5 +20,32 @@ public class NoteInteractor : MonoBehaviour
     public void SendImpulse()
     {
         controller.SendHapticImpulse(1.0f, 0.1f);
+    }
+    public void TriggerHitEffect(Vector3 position)
+    {
+        // 본인 로컬 이펙트
+        PlayHitEffect(position);
+
+        // 다른 클라이언트에 이펙트 전달
+        if (photonView.IsMine)
+        {
+            photonView.RPC(nameof(RPC_PlayHitEffect), RpcTarget.Others, position);
+        }
+    }
+
+    [PunRPC]
+    private void RPC_PlayHitEffect(Vector3 position)
+    {
+        PlayHitEffect(position);
+    }
+
+    private void PlayHitEffect(Vector3 position)
+    {
+        if (hitFXPrefab != null)
+        {
+            var effect = PoolManager.Instance.Spawn<ParticleSystem>(hitFXPrefab, position, Quaternion.identity);
+            effect.Play();
+            PoolManager.Instance.Despawn(effect, 2.0f);
+        }
     }
 }
